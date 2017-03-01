cc632922c2a2e237b63f28e074dc94e2539f19f2
mohamed-ismat <mnb1236@live.com>
2017-03-01 13:54:27 +0300

    added first hub

---
+++ ChatSignalR/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ChatSignalR
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        
+        public void SendMessage(string message)
+        {
+            Clients.All.hello();
+        }
+    }
+}
