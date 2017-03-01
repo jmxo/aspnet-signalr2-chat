7f532bd435a4aad82a43e8aaa042df35b7d85421
mohamed-ismat <mnb1236@live.com>
2017-03-01 15:42:34 +0300

    implemeted group functionality

--- ChatSignalR/ChatHub.cs
+++ ChatSignalR/ChatHub.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
 
 namespace ChatSignalR
 {
@@ -13,7 +14,45 @@ namespace ChatSignalR
         
         public void SendMessage(string message)
         {
-            Clients.All.hello();
+            var msg = string.Format("{0}: {1}", Context.ConnectionId, message);
+
+            Clients.All.newMessage(msg);
+
+            //Same
+            //Clients.Caller.newMessage(message);
+            //Clients.Client(Context.ConnectionId).newMessage(message);
+
+            //Same
+            //Clients.Others
+            //Clients.AllExcept(Context.ConnectionId)
         }
+
+        public void JoinRoom(string room)
+        {
+            // Note: groups are not persisted on the server
+            Groups.Add(Context.ConnectionId, room);
+        }
+        
+        public void SendMessageToRoom (string room, string message)
+        {
+            var msg = string.Format("{0}: {1}", Context.ConnectionId, message);
+            Clients.Group(room).newMessage(msg);
+        }    
+
+        public void SendMessageData(SendData data)
+        {
+            Clients.All.newData();
+        }
+
+        //public Task<int> SendDataAsync()
+        //{
+        //    //async work
+        //}
+    }
+
+    public class SendData
+    {
+        public int ID { get; set; }
+        public string Data { get; set; }
     }
 }
